c6b6348c8f2190501dd8ca6d3132c2f70faa51f4
Rakitin <drakitin@pmintl.net>
2026-02-09 16:32:08 +0400

    Fix keys in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://supabase.com/dashboard/project/sondbwdrdgvzjtjdslaq!
-const supabaseAnonKey = process.env.sb_publishable_pcwqhodDtNzs2CpvbZROpQ_D_JW6Bxh!
+const supabaseUrl = 'https://supabase.com/dashboard/project/sondbwdrdgvzjtjdslaq'
+const supabaseAnonKey = 'sb_publishable_pcwqhodDtNzs2CpvbZROpQ_D_JW6Bxh'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
